ffd21bda37a932faf175c77d977372c4466f9453
Mateusz Lapies <mat.lapies@gmail.com>
2022-01-21 20:00:04 +0100

    frontend
    base

---
+++ frontend/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+      
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
